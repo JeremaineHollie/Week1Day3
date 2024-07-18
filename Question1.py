704378bffe560b25d5f496de9fdf1afb3830cf09
JeremaineHollie <jeremaine52@gmail.com>
2024-07-18 15:18:45 -0500

    first commit

---
+++ Question1.py
@@ -0,0 +1,13 @@
+# Task 1
+
+# grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+grades.reverse()
+print(grades)
+
+# Task 2
+
+Average_grade = sum(grades) / len(grades)
+print("Average Grade: ", Average_grade)
